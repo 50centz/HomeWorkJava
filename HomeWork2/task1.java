a25717c08f0bf565ee01263648c535eb63430dd7
Федоров Евгений <zen-g@mail.ru>
2023-02-15 17:52:41 +0300

    Work to home

--- HomeWork2/task1.java
+++ HomeWork2/task1.java
@@ -1,6 +1,9 @@
 package HomeWork.HomeWork2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Arrays;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -12,29 +15,48 @@ public class task1 {
         //     Если значение null, то параметр не должен попадать в запрос.
         //     Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
         StringBuilder sqlZapros = new StringBuilder("select * from students where");
-        JSONObject data =  createJson();
+        createJson();
+        String getjsonFile = get_json_file("C:/Users/e.fedorov/Desktop/Java/HomeWork/HomeWork2/filetask1.json");
+        //System.out.println(getjsonFile);
         // sqlZapros = stroka(sqlZapros, data);
         // System.out.println(sqlZapros);
     }
 
-    public static JSONObject createJson(){
-        JSONObject jsonStroka = new JSONObject();
-        jsonStroka.put("name", "Ivanov");
-        jsonStroka.put("country", "Russia");
-        jsonStroka.put("city", "Moscow");
-        jsonStroka.put("age", "null");
-        System.out.println(jsonStroka);
-        try (FileWriter mywriter = new FileWriter("C:/Users/e.fedorov/Desktop/Java/HomeWork/HomeWork2/file.json")){
-            mywriter.write(String.valueOf(jsonStroka));
+    public static void createJson(){
+        JSONObject jsonFile = new JSONObject();
+        jsonFile.put("name", "Ivanov");
+        jsonFile.put("country", "Russia");
+        jsonFile.put("city", "Moscow");
+        jsonFile.put("age", null);
+        System.out.println(jsonFile);
+        try (FileWriter mywriter = new FileWriter("C:/Users/e.fedorov/Desktop/Java/HomeWork/HomeWork2/filetask1.json")){
+            mywriter.write(String.valueOf(jsonFile));
         } catch (Exception e){
             System.out.println("Ошибка");
         }
         finally{
             System.out.println("Конец работы с файлом");
         }
-        return jsonStroka;
     }
     
+    public static String get_json_file(String filename){
+        String jsonfileget = "";
+        
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                jsonfileget += line + "\n";
+            }
+            bufferedReader.close();
+            System.out.println(jsonfileget.getClass());
+            System.out.println(jsonfileget.toString());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return jsonfileget;
+
+    }
 
 
 
