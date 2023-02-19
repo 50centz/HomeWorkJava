c36ae4f9ffd8bbe8a7260fd9388300ef73c3b2d4
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-02-19 20:26:04 +0300

    Solved first task

--- HomeWork3/Task1.java
+++ HomeWork3/Task1.java
@@ -1,5 +1,8 @@
 package HomeWorkJava.HomeWork3;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Task1
  */
@@ -7,6 +10,54 @@ public class Task1 {
 
     public static void main(String[] args) {
         // 1.Реализовать алгоритм сортировки слиянием
+        ArrayList <String> firstList = createFirstList();
+        ArrayList <String> secondList = createSecondList();
+        ArrayList<ArrayList<String>> cars = new ArrayList<>(create2DList(firstList, secondList));
+        ArrayList<String> mergingSheets = mergingSheets(cars);
+        System.out.println(mergingSheets.toString());
         
+    }
+
+   
+
+    public static ArrayList <String> createFirstList(){
+
+        ArrayList <String> firstList = new ArrayList<>();
+        firstList.add("Toyota");
+        firstList.add("Mazda");
+        firstList.add("Nissan");
+
+        return firstList;
+    }
+
+    public static ArrayList <String> createSecondList(){
+
+        ArrayList <String> secondList = new ArrayList<>();
+        secondList.add("Audi");
+        secondList.add("BMW");
+        secondList.add("VW");
+
+        return secondList;
+    }
+
+    public static ArrayList<ArrayList<String>> create2DList(ArrayList <String> firstList, ArrayList <String> secondList){
+
+        ArrayList<ArrayList<String>> cars = new ArrayList<>();
+        cars.add(firstList);
+        cars.add(secondList);
+
+        return cars;
+    }
+
+    public static ArrayList<String> mergingSheets(ArrayList<ArrayList<String>> cars){
+
+        ArrayList <String> mergingSheets = new ArrayList<>();
+        ArrayList <String> firstList = new ArrayList<>(cars.get(0));
+        ArrayList <String> secondList = new ArrayList<>(cars.get(1));
+
+        mergingSheets.addAll(firstList);
+        mergingSheets.addAll(secondList);
+    
+        return mergingSheets;
     }
 }
