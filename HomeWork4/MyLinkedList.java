96d879c43c2be9229c3cbe352df7ace38bdfa182
Федоров Евгений <zen-g@mail.ru>
2023-02-21 17:09:28 +0300

    Work to home

---
+++ HomeWork4/MyLinkedList.java
@@ -0,0 +1,18 @@
+package HomeWork.HomeWork4;
+
+import java.util.LinkedList;
+
+public class MyLinkedList {
+    private LinkedList<Integer> ll = new LinkedList<>(); // поля
+    private int length = 0;
+
+
+    int size() {
+        return length;
+    }
+
+    void add(int item) {
+        ll.add(length);
+    }
+
+}
---
+++ HomeWork4/task1.java
@@ -0,0 +1,13 @@
+package HomeWork.HomeWork4;
+
+public class task1 {
+    public static void main(String[] args) {
+        // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список. Постараться не обращаться к листу по индексам. 
+
+        MyLinkedList ll = new MyLinkedList();
+        ll.add(5);
+        System.out.println(ll.size());
+    }
+
+    
+}
