86c2697b47da2d73be9638c83312f4d32334a19b
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-02-26 14:39:50 +0300

    Edited second task

--- HomeWork5/Task2.java
+++ HomeWork5/Task2.java
@@ -1,6 +1,6 @@
 package HomeWorkJava.HomeWork5;
 
-import java.util.Arrays;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Task2 {
@@ -15,30 +15,45 @@ public class Task2 {
 
         String s = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова" + "," +
         "Иван Юрин, Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова," + 
-        "Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов, Иван Иванов, Павел Чернов";
+        "Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов";
         TreeMap<String, Integer> tmap =  sortInDescending(s);
         System.out.println(tmap);
     }
 
     public static TreeMap<String, Integer> sortInDescending(String s){
 
-        TreeMap<String, Integer> treem = new TreeMap<>();
+        Map<String, Integer> tmap = new TreeMap<>();
 
         String[] s1 = s.split(",");
-        // System.out.println(Arrays.toString(s1));
+        // System.out.println(Arrays.toString(s1));           // в этом цикле получаем Имя и Фамилию как ключ, а значение количество
         // for (int i = 0; i < s1.length; i++) {
         //     System.out.println(s1[i].trim());
         // }
-        int count = 1;  
+        // int count = 1;  
+        // for (int i = 0; i < s1.length; i++) {
+        //     if(treem.containsKey(s1[i].trim())){
+        //         int a = treem.get(s1[i].trim());
+        //         a += 1;
+        //         treem.put(s1[i].trim(), a);
+        //     }else{
+        //         treem.put(s1[i].trim(), count);
+        //     }
+        // }
+
+
+        int count = 1;
         for (int i = 0; i < s1.length; i++) {
-            if(treem.containsKey(s1[i].trim())){
-                int a = treem.get(s1[i].trim());
+            String s2 = s1[i].trim();
+            String[] s3 = s2.split(" ");
+            if(tmap.containsKey(s3[0])){
+                int a = tmap.get(s3[0]);
                 a += 1;
-                treem.put(s1[i].trim(), a);
+                tmap.put(s3[0], a);
             }else{
-                treem.put(s1[i].trim(), count);
+                tmap.put(s3[0], count);
             }
+            
         }
-        return treem;
+        return (TreeMap<String, Integer>) tmap;
     }
 }
